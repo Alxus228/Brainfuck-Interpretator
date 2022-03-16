f839427454ef06a3bdd0f4f85c392e8849e40f55
Alxus228 <rya111222@gmail.com>
2022-03-16 09:50:05 +0200

    Adding tests

--- brainfuck/brainfuck_test.go
+++ brainfuck/brainfuck_test.go
@@ -11,13 +11,13 @@ func TestInterpretate(t *testing.T) {
 	r, w, _ := os.Pipe()
 	os.Stdout = w
 
-	Interpetate("+++++c>p++++++[-<+>]<.")
+	Interpetate("+++++c>p+++++[-<+>]<.")
 
 	w.Close()
 	out, _ := ioutil.ReadAll(r)
 	os.Stdout = rescueStdout
 
-	if out[0] != 16 {
-		t.Errorf("The output of programm +++++c>p+++++[-<+>]<. should be 16, but it equals %d", out[0])
+	if out[0] != 15 {
+		t.Errorf("The output of programm +++++c>p+++++[-<+>]<. should be 15, but it equals %d", out[0])
 	}
 }
