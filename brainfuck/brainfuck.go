f8fad37a0489cd7a8f56ca8d2aaa828fe960ec73
Alxus228 <rya111222@gmail.com>
2022-03-20 12:38:11 +0200

    Full Documentation Evey type and function in brainfuck package has a documentation now. Also added comments where the code isn't obvious.

--- brainfuck/brainfuck.go
+++ brainfuck/brainfuck.go
@@ -1,60 +1,79 @@
 package brainfuck
 
+// This map contains empty structures that inherit command interface
 var executableCommands = map[rune]command{
-	//Increment operation
+	// increment
 	'+': increment{},
-	//Decrement operation
+	// decrement
 	'-': decrement{},
-	//Increment data pointer operation
+	// increment data pointer
 	'>': incrementDataPointer{},
-	//Decrement data pointer operation
+	// decrement data pointer
 	'<': decrementDataPointer{},
-	//Output operation
+	// output
 	'.': output{},
-	//Input operation
+	// input
 	',': input{},
-	//The beginning of loop
+	// the beginning of loop
 	'[': loop{},
-	//The end of loop
+	// the end of loop
 	']': endLoop{},
-	//All the functions implemented after the ']', are not implemented in original Brainfuck language
-	//Copy operation
+	// All the functions after the ']', are not implemented in original Brainfuck language
+	// copy
 	'c': copy{},
-	//Paste operation
+	// paste
 	'p': paste{},
-	//Clear operation
+	// clear
 	'0': zero{},
 }
 
+// The memmory set we're going to use everywhere.
 var memmorySet memmory
-var codePointer int
 
+// This variable is needed for copy and paste commands and can't
+// be deleted or renamed without modification of those commands.
 var copyPasteAccumulator byte
+
+// This slice of loops we're going to use as a stack for
+// loop commands in which we will append commands inside "[]".
+//
+// After the interpretation we will execute it, but not with the
+// currentLoop[0].execute(memmorySet), because we don't need to repeat it
 var currentLoop = []loop{
-	{},
+	{}, // this is the main loop
 }
 
+// Brainfuck function interpretates and compiles brainfuck code.
+//
+// Specification of the language can be found on Wiki: https://en.wikipedia.org/wiki/Brainfuck.
 func Brainfuck(code string) {
 	interpretate(code)
 	compile()
 }
 
-//function to call
+// Function interpretate transforms string into commands and appends them into the main loop
 func interpretate(code string) {
-	for codePointer = 0; codePointer < len(code); codePointer++ {
+	for codePointer := 0; codePointer < len(code); codePointer++ {
 		var newCommand = executableCommands[rune(code[codePointer])]
+		// depending on the type we will process commands differently
 		switch t := newCommand.(type) {
 		case loop:
+			// push new loop into the top of stack
 			currentLoop = append([]loop{t}, currentLoop...)
 		case endLoop:
+			// append the current loop into the innerLoop of the outer loop
+			// if there's no outer loop, it will be appended to the main loop
 			currentLoop[1].innerLoop = append(currentLoop[1].innerLoop, currentLoop[0])
+			// pop current loop
 			currentLoop = currentLoop[1:]
 		default:
+			// append a new command into the innerLoop of the current loop
 			currentLoop[0].innerLoop = append(currentLoop[0].innerLoop, newCommand)
 		}
 	}
 }
 
+// Function compile execute all commands from the main loop.
 func compile() {
 	for _, com := range currentLoop[0].innerLoop {
 		com.execute(&memmorySet)
