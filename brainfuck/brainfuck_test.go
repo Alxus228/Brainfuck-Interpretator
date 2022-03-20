c543db1fe870bcf6ccad2a0c5353608bf4a2b656
Alxus228 <rya111222@gmail.com>
2022-03-20 16:29:13 +0200

    Added '+' '-' '>' '<' execute() tests Added protection against cellsSize

--- brainfuck/brainfuck_test.go
+++ brainfuck/brainfuck_test.go
@@ -22,3 +22,33 @@ func TestBrainfuck(t *testing.T) {
 		t.Errorf("The output of programm +++++c>p+++++[-<+>]<. should be 16, but it equals %d", out[0])
 	}
 }
+
+func TestExecute(t *testing.T) {
+	mem := memmory{}
+
+	executeTests := []struct {
+		com             command
+		pointer         int
+		value, expected byte
+	}{
+		{new(increment), 0, 0, 1},
+		{new(increment), 0, 255, 0},
+		{new(decrement), 0, 0, 255},
+		{new(decrement), 1, 5, 4},
+		{new(incrementDataPointer), 0, 0, 4},
+		{new(incrementDataPointer), 199, 10, 0},
+		{new(decrementDataPointer), 0, 0, 10},
+		{new(decrementDataPointer), 2, 0, 4},
+	}
+
+	for _, test := range executeTests {
+		mem.pointer = test.pointer
+		mem.cells[mem.pointer] = test.value
+
+		test.com.execute(&mem)
+
+		if mem.cells[mem.pointer] != test.expected {
+			t.Errorf("Expected %d, but got %d", test.expected, mem.cells[mem.pointer])
+		}
+	}
+}
